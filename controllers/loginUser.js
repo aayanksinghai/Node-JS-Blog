ebd13f1fddbf27917e0fbb6dafcba0a39dbfcc9a
aayanksinghai <aayank.singhai@scool.in>
2020-09-06 20:55:17 +0530

    Session Auth Middleware

---
+++ controllers/loginUser.js
@@ -0,0 +1,25 @@
+const User = require('../database/models/User')
+const bcrypt = require('bcrypt')
+
+module.exports = (req, res) => {
+const { email, password } = req.body
+User.findOne({ email: email}, (error, user) => {
+    if(user)
+    {
+        bcrypt.compare(password, user.password, (error, result) => {
+            if(result)
+            {
+                req.session.userId = user._id
+                res.redirect('/')
+            }
+            else{
+                res.redirect('/auth/login')
+            }
+        })
+    }
+    else
+    {
+        res.redirect('/auth/login')
+    }
+})
+}
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const {engine} = require('express-edge')
 const mongoose = require('mongoose')
+const expressSession = require('express-session')
+const connectMongo = require('connect-mongo')
 const fileUpload = require('express-fileupload')
 const bodyParser = require('body-parser')
 
@@ -12,13 +14,26 @@ const storePostController = require('./controllers/storePost')
 const getPostController = require('./controllers/getPost')
 const createUserController = require('./controllers/createUser')
 const storeUserController = require('./controllers/storeUser')
+const loginController = require('./controllers/login')
+const loginUserController = require('./controllers/loginUser')
 
 //Models
 const Post = require('./database/models/Post')
 
 const app = express()
-
 mongoose.connect('mongodb://localhost/node-js-blog')
+
+//Sessions
+const mongoStore = connectMongo(expressSession)
+
+app.use(expressSession({
+    secret: 'secret',
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}))
+
+
 
 app.use(express.static('public'))
 app.use(engine)
@@ -27,18 +42,23 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 const validateCreatePostMiddleware = require('./middleware/storePost')
+const auth = require('./middleware/auth')
+const storePost = require('./middleware/storePost')
 
-app.use('/posts/store', validateCreatePostMiddleware)
+
+
 
 app.set('views', `${__dirname}/views`);
 
 app.get('/', homePageController)
 app.get('/auth/register', createUserController)
-app.get('/posts/new', createPostController)
-app.post('/posts/store', storePostController)
+app.get('/posts/new', auth, createPostController)
+app.post('/posts/store', auth, storePost, storePostController)
 app.get('/post/:id', getPostController)
-
+app.get('/auth/login', loginController)
 app.post('/users/register', storeUserController)
+app.post('/users/login', loginUserController)
+
 app.listen(4000, () => {
     console.log('App listening on port 4000!');
 }); 
