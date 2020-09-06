c4d567af45d807f4ffc0fec1331b548de3703ac8
aayanksinghai <aayank.singhai@scool.in>
2020-09-06 15:42:05 +0530

    MVC Implemented

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const {engine} = require('express-edge')
 const mongoose = require('mongoose')
 const fileUpload = require('express-fileupload')
 const bodyParser = require('body-parser')
+
+//Controllers
+const createPostController = require('./controllers/createPost')
+const homePageController = require('./controllers/homepage')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
+
+//Models
 const Post = require('./database/models/Post')
 
 const app = express()
-
-
 
 mongoose.connect('mongodb://localhost/node-js-blog')
 
@@ -30,47 +36,10 @@ app.use('/posts/store', validateCreatePostMiddleware)
 
 app.set('views', `${__dirname}/views`);
 
-app.get('/', async (req,res) => {
-    const posts = await Post.find({})
-    console.log(posts)
-    res.render('index', {
-        posts
-    }) 
-})
-app.get('/posts/new', (req, res) =>
-{
-    res.render('create')
-})
-
-app.post('/posts/store', (req,res) => {
-    const { image } = req.files
-    image.mv(path.resolve(__dirname,'public/posts', image.name), (error) => {
-        Post.create({
-            ...req.body,
-            image: `/posts/${image.name}`
-        }, (error, post) => {
-            res.redirect('/')
-        })
-    })
-    
- 
-})
-
-app.get('/about',(req,res) => {
-    res.render('about')
-})
-
-app.get('/post/:id', async (req,res) => {
-    const post = await Post.findById(req.params.id)
-    res.render('post', {
-        post
-    })
-})
-
-app.get('/contact',(req,res) => {
-    res.render('contact')
-})
-
+app.get('/', homePageController)
+app.get('/posts/new', createPostController)
+app.post('/posts/store', storePostController)
+app.get('/post/:id', getPostController)
 
 app.listen(4000, () => {
     console.log('App listening on port 4000!');
